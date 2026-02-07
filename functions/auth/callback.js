ee0eaa14c86d9428d157b5f57abdbda982dd1a3e
sammassengale82 <valorwaveentertainment@gmail.com>
2026-02-06 23:42:58 -0500

    Update callback.js

--- functions/auth/callback.js
+++ functions/auth/callback.js
@@ -1,27 +1,41 @@
 export async function onRequestGet(context) {
   const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = context.env;
   const code = new URL(context.request.url).searchParams.get("code");
+  
   const response = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
     headers: { "content-type": "application/json", "accept": "application/json" },
     body: JSON.stringify({ client_id: GITHUB_CLIENT_ID, client_secret: GITHUB_CLIENT_SECRET, code }),
   });
+  
   const result = await response.json();
-  const token = result.access_token;
 
-  return new Response(`
+  // This is the "Magic" string that tells Static CMS the login worked
+  const content = `
+    <!DOCTYPE html>
     <html>
+    <head><title>Authorizing...</title></head>
     <body>
       <script>
         (function() {
-          const message = "authorization:github:success:" + JSON.stringify({
-            token: "${token}",
+          function receiveMessage(e) {
+            console.log("Receiving message:", e.data);
+          }
+          window.addEventListener("message", receiveMessage, false);
+          
+          const response = {
+            token: "${result.access_token}",
             provider: "github"
-          });
-          window.opener.postMessage(message, "*");
-          window.close();
+          };
+          
+          window.opener.postMessage(
+            "authorization:github:success:" + JSON.stringify(response),
+            window.location.origin
+          );
         })();
       </script>
     </body>
-    </html>`, { headers: { "content-type": "text/html" } });
+    </html>`;
+
+  return new Response(content, { headers: { "content-type": "text/html" } });
 }
