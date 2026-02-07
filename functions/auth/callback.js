aa9480107d32b42a1bd2d5c13af39ca37ae58530
sammassengale82 <valorwaveentertainment@gmail.com>
2026-02-06 23:23:55 -0500

    Update callback.js

--- functions/auth/callback.js
+++ functions/auth/callback.js
@@ -1,17 +1,14 @@
 export async function onRequestGet(context) {
   const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = context.env;
   const code = new URL(context.request.url).searchParams.get("code");
-  
   const response = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
     headers: { "content-type": "application/json", "accept": "application/json" },
     body: JSON.stringify({ client_id: GITHUB_CLIENT_ID, client_secret: GITHUB_CLIENT_SECRET, code }),
   });
-  
   const result = await response.json();
   const token = result.access_token;
 
-  // We are simplifying the response to avoid the "Unexpected Identifier" error
   return new Response(`
     <html>
     <body>
