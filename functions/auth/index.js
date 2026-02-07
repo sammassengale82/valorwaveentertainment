e77ed1ef52fd23781ce699b02d13ea56e773d435
sammassengale82 <valorwaveentertainment@gmail.com>
2026-02-06 22:37:18 -0500

    Update index.js

--- functions/auth/index.js
+++ functions/auth/index.js
@@ -1,1 +1,5 @@
-
+export async function onRequestGet(context) {
+  const { GITHUB_CLIENT_ID } = context.env;
+  const url = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=repo,user`;
+  return Response.redirect(url);
+}
