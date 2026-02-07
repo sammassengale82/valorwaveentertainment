41fb1c141665e3ae86f82a8cf2b641006dd5afc6
sammassengale82 <valorwaveentertainment@gmail.com>
2026-02-07 14:31:09 -0500

    Update callback.js

--- functions/auth/callback.js
+++ functions/auth/callback.js
@@ -5,18 +5,15 @@ export async function onRequestGet(context) {
   const response = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
     headers: { "content-type": "application/json", "accept": "application/json" },
-    body: JSON.stringify({ 
-      client_id: GITHUB_CLIENT_ID, 
-      client_secret: GITHUB_CLIENT_SECRET, 
-      code 
-    }),
+    body: JSON.stringify({ client_id: GITHUB_CLIENT_ID, client_secret: GITHUB_CLIENT_SECRET, code }),
   });
   
   const result = await response.json();
 
-  return new Response(`
+  const html = `
     <!DOCTYPE html>
     <html>
+    <head><meta charset="UTF-8"></head>
     <body>
       <script>
         (function() {
@@ -24,13 +21,13 @@ export async function onRequestGet(context) {
             token: "${result.access_token}",
             provider: "github"
           });
-          // Using "*" allows the message to cross between www. and non-www. versions
-          if (window.opener) {
-            window.opener.postMessage(message, "*");
-            window.close();
-          }
+          // We target the specific domain to satisfy CORB/CORS requirements
+          window.opener.postMessage(message, "https://valorwaveentertainment.com");
+          window.close();
         })();
       </script>
     </body>
-    </html>`, { headers: { "content-type": "text/html" } });
+    </html>`;
+
+  return new Response(html, { headers: { "content-type": "text/html" } });
 }
