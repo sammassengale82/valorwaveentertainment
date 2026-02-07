cb363d6cb136290d12b7a05b9abac123279f8a08
sammassengale82 <valorwaveentertainment@gmail.com>
2026-02-07 15:24:57 -0500

    Update callback.js

--- functions/auth/callback.js
+++ functions/auth/callback.js
@@ -1,33 +1,51 @@
 export async function onRequestGet(context) {
-  const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = context.env;
-  const code = new URL(context.request.url).searchParams.get("code");
-  
-  const response = await fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    headers: { "content-type": "application/json", "accept": "application/json" },
-    body: JSON.stringify({ 
-      client_id: GITHUB_CLIENT_ID, 
-      client_secret: GITHUB_CLIENT_SECRET, 
-      code 
-    }),
-  });
-  
-  const result = await response.json();
 
-  // The simplified HTML below is less likely to trigger CORB
-  return new Response(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head><meta charset="utf-8"></head>
-    <body>
-      <script>
-        const message = "authorization:github:success:" + JSON.stringify({
-          token: "${result.access_token}",
-          provider: "github"
-        });
-        window.opener.postMessage(message, "https://valorwaveentertainment.com");
-        window.close();
-      </script>
-    </body>
-    </html>`, { headers: { "Content-Type": "text/html" } });
+const { searchParams } = new URL(context.request.url);
+
+const code = searchParams.get("code");
+
+const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = context.env;
+
+if (!code) return new Response("Error: No code received", { status: 400 });
+
+try {
+
+const response = await fetch("", {
+
+method: "POST",
+
+headers: {
+
+"content-type": "application/json",
+
+"user-agent": "cloudflare-pages-static-cms",
+
+accept: "application/json",
+
+},
+
+body: JSON.stringify({
+
+client_id: GITHUB_CLIENT_ID,
+
+client_secret: GITHUB_CLIENT_SECRET,
+
+code,
+
+}),
+
+});
+
+const result = await response.json();
+
+const content = <html><body><script>(function(){const token="${result.access_token}";const message="authorization:github:success:"+JSON.stringify({token:token,provider:"github"});window.opener.postMessage(message,"https://valorwaveentertainment.com");window.close();})()</script></body></html>;
+
+return new Response(content, { headers: { "content-type": "text/html;charset=UTF-8" } });
+
+} catch (error) {
+
+return new Response(error.message, { status: 500 });
+
+}
+
 }
