86fb805280fc9b1c71a22fb16801e29feffc026c
sammassengale82 <valorwaveentertainment@gmail.com>
2026-02-07 15:25:32 -0500

    Update index.js

--- functions/auth/index.js
+++ functions/auth/index.js
@@ -1,10 +1,9 @@
 export async function onRequestGet(context) {
-  const { GITHUB_CLIENT_ID } = context.env;
-  const url = new URL("https://github.com/login/oauth/authorize");
-  url.searchParams.set("client_id", GITHUB_CLIENT_ID);
-  url.searchParams.set("scope", "repo,user");
-  // Explicitly setting the redirect ensures the handshake is valid
-  url.searchParams.set("redirect_uri", "https://valorwaveentertainment.com/auth/callback");
 
-  return Response.redirect(url.href, 302);
+const { GITHUB_CLIENT_ID } = context.env;
+
+const githubAuthUrl = https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=repo,user;
+
+return Response.redirect(githubAuthUrl);
+
 }
