1f2d64814beb2e971a8911fdc63d82bac1243b9f
sammassengale82 <valorwaveentertainment@gmail.com>
2026-02-07 23:51:12 -0500

    Delete functions/auth directory

--- functions/auth/index.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet(context) {
-
-const { GITHUB_CLIENT_ID } = context.env;
-
-const githubAuthUrl = https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=repo,user;
-
-return Response.redirect(githubAuthUrl);
-
-}
