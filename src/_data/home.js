3b60e73017eec93f79e2a698a7627a85f922ed26
sammassengale82 <valorwaveentertainment@gmail.com>
2026-02-08 18:34:38 -0500

    Update home.js

--- src/_data/home.js
+++ src/_data/home.js
@@ -1,8 +1,36 @@
+const yaml = require("js-yaml");
 const fs = require("fs");
-const matter = require("gray-matter");
+const path = require("path");
 
 module.exports = () => {
-  const file = fs.readFileSync("src/content/pages/home.md", "utf8");
-  const { data } = matter(file);
-  return data;
+  const filePath = path.join(__dirname, "../content/pages/home.md");
+  const raw = fs.readFileSync(filePath, "utf8");
+
+  // Extract frontmatter
+  const match = raw.match(/---([\s\S]*?)---/);
+  if (!match) return {};
+
+  const data = yaml.load(match[1]);
+
+  return {
+    title: data.title,
+    hero_kicker: data.hero_kicker,
+    hero_tagline: data.hero_tagline,
+    hero_subline: data.hero_subline,
+    hero_button: data.hero_button,
+
+    services_title: data.services_title,
+    services_footer: data.services_footer,
+
+    bio_title: data.bio_title,
+    bio_name: data.bio_name,
+    bio_image: data.bio_image,
+    bio_text: data.bio_text,
+
+    testimonials_title: data.testimonials_title,
+
+    contact_title: data.contact_title,
+    contact_form_action: data.contact_form_action,
+    contact_button: data.contact_button
+  };
 };
