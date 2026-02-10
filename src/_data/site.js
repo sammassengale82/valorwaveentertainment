9e581b98cf3614b087ff0c12b932bd26d7c2576d
sammassengale82 <valorwaveentertainment@gmail.com>
2026-02-10 03:50:46 -0500

    Delete src/_data directory

--- src/_data/site.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const matter = require("gray-matter");
-
-module.exports = () => {
-  const file = fs.readFileSync("src/content/settings/site.md", "utf8");
-  const { data } = matter(file);
-  return data;
-};
