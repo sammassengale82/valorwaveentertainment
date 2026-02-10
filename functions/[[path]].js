6284ca822ab30a1df8e34b5097f28449cf23ed42
sammassengale82 <valorwaveentertainment@gmail.com>
2026-02-10 04:09:49 -0500

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,11 +1,9 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
 
-  // Only proxy OAuth routes to your Worker
   if (url.pathname.startsWith("/oauth/")) {
     return context.env.jolly_voice_2fd4.fetch(context.request);
   }
 
-  // Everything else: serve static site normally
   return context.next();
 }
